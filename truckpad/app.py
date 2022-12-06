15ae4b9cf3640dbe2bba48e1d95df7006790a47f
Leonardo Prado <leonardoprado@gmail.com>
2022-12-06 13:41:41 -0300

    Remove custom error handler
    Signed-off-by: Leonardo Prado <leonardoprado@gmail.com>

--- truckpad/app.py
+++ truckpad/app.py
@@ -9,10 +9,6 @@ class Base:
     async def on_get(self, req, res):
         res.media = {'hello': 'world'}
 
-async def handle_uncaught_exception(req, resp, ex, params):
-    logging.exception('Unhandled error')
-    raise falcon.HTTPInternalServerError(title='App error')
-
 
 def create_app(config=None, mongodb=None):
     config = config or Config()
@@ -21,7 +17,6 @@ def create_app(config=None, mongodb=None):
     app = falcon.asgi.App()
     app.add_route('/', Base())
     app.add_route('/drivers', Drivers())
-    app.add_error_handler(Exception, handle_uncaught_exception)
 
     return app
 
